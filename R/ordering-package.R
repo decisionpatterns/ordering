7b447798d15d794c8dc53907167ab8447a738f28
Christopher Brown <cbrown@decisionpatterns.com>
2018-11-09 15:13:58 -0800

    Update ordering=package.R

---
+++ R/ordering-package.R
@@ -0,0 +1,19 @@
+#' @name ordering-package
+#'
+#' @aliases ordering-package, ordering
+#' @rdname ordering-package
+#' 
+#' @docType package
+#' 
+#' @title Check/verify/confirm the ordering of a vector
+#'
+#' @description
+#' This packages provides a number of useful utilities for checking/verifying/ 
+#' confirming the ordering of a vector. It can determine whether
+#' values are constant, monotonic, or strictly increasing/decreasing.  
+#' It contains options for handling \code{NA} values as well as many 
+#' convenience and efficient functions. 
+#'
+#' @author Christopher Brown
+  
+NULL
